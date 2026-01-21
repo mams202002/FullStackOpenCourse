ef4cc30d6392bb05fad07fe510bc6b245cf1ba9b
Mouhamadou MBAYE <mams202002@yahoo.fr>
2026-01-21 15:17:41 +0100

    part1 step3 completed

--- part1/course_information/src/App.jsx
+++ part1/course_information/src/App.jsx
@@ -1,54 +1,57 @@
+const Header = (props) => {
+  console.log(props)
+  return  <h1>{props.course}</h1>     
+}
+const Part = (props) => {
+  console.log(props)
+  return <p>{props.part} {props.exercise}</p>
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1.name} exercise={props.part1.exercise} />
+      <Part part={props.part2.name}  exercise={props.part2.exercise}/>
+      <Part part={props.part3.name} exercise={props.part3.exercise}  />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+     <p>Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}</p> 
+  )
+}
+
+
+
+
 const App = () => {
-  
-
-  return (
-    <div>
-      <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
-    </div>
-
-  )
-}
-
-const Header = () => {
   const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercise: 10
+  } 
+  const part2 = {
+    name: 'Using props to pass data',
+    exercise: 7
+  } 
+  
+  const part3 = {
+    name: 'State of a component',
+    exercise: 14
+  } 
+  
   return (
     <div>
-      <h1>{course}</h1>
+      <Header  course={course}/>
+      <Content part1={part1} part2={part2} part3 = {part3}  />
+      <Total exercise1={part1.exercise} exercise2={part2.exercise} exercise3={part3.exercise} />
     </div>
   )
 }
-const Content = () => {
-  const part1 = 'Fundamentals of React'
-  const part2 = 'Using props to pass data'
-  const part3 = 'State of a component'
-  const exercise1 = 10
-  const exercise2 = 7
-  const exercise3 = 14
-  return (
-    <div>
-      <p>
-        {part1} {exercise1}
-      </p>
-      <p>
-        {part2} {exercise2}
-      </p>
-      <p>
-        {part3} {exercise3}
-      </p>
 
-    </div>
 
-  )
 
-}
-
-const Total = () => {
-  return (
-    <div>
-      <Content /> 
-    </div>
-  )
-
-}
-
-export default Total
+export default App
