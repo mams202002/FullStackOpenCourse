bd41a4c31260db45cda1bc5550771fcde1193b7f
Mouhamadou MBAYE <mams202002@yahoo.fr>
2026-02-16 11:48:14 +0100

    unicafe step1 and step2 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,58 @@
 import { useState } from 'react'
 
 const App = () => {
+
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
 
+  const click_good = () => {
+    setGood(good + 1)
+  }
+  const click_neutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const click_bad = () => {
+    setBad(bad + 1)
+  }
+
+  const all = good + bad + neutral
+  const positive = all > 0 ? ((good / all) * 100).toFixed(13) : 0
+  const average = all > 0 ? ((good - bad)/all).toFixed(15) : 0
+
+  
+
+  
+ 
   return (
     <div>
-      This is exactly where my code goes
+      <h1>give feedback</h1>
+      <button onClick={click_good}>good</button>
+      <button onClick={click_neutral}>neutral</button>
+      <button onClick={click_bad}>bad</button>
+      <h1>statistics</h1>
+      <div>
+        <a>good {good}</a>
+      </div>
+      <div>
+        <a>neutral {neutral}</a>
+      </div> 
+      <div>
+        <a>bad {bad}</a>
+      </div>
+      <div>
+        <a>all {all}</a>
+      </div> 
+      <div>
+        <a>average {average} </a>
+      </div> 
+      <div>
+        <a>positive {positive} %</a>
+      </div>    
+
+
     </div>
   )
 }
