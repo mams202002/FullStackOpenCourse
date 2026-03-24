323a3f7cb2bc01f725a3f155241d6d2c1545e3fd
Mouhamadou MBAYE <mams202002@yahoo.fr>
2026-03-24 12:29:44 +0100

    unicafe step6 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,37 +2,46 @@ import { useState } from 'react'
 
 
 const StatisticLine = ({text, value}) => {
+  
   return (
-    <p>{text} {value} </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+    
   )
 }
 
 
-const Statistics = ({good, neutral, bad, all, average, positive}) => {
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + bad + neutral
+  const positive = all > 0 ? ((good / all) * 100) : 0
+  const average = all > 0 ? ((good - bad)/all) : 0
   if (all === 0 ) return (
-    <div>
-        <h1>statistics</h1>
         <p>No feedback given</p>
-      </div>
   )
   return (
 
-    <div>
-      <h1>statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive + "%"} />
-      
-    </div>
+
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average.toFixed(2)} />
+        <StatisticLine text="positive" value={positive.toFixed(2) + "%"} />
+      </tbody>
+    </table>
+
+    
     
   )
 
 }
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Button = ({handleClick, text}) => (
+<button onClick={handleClick}>{text}</button>)
 
 
 const App = () => {
@@ -43,38 +52,26 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
 
-  const click_good = () => {
-    setGood(good + 1)
-  }
-  const click_neutral = () => {
-    setNeutral(neutral + 1)
-  }
-  const click_bad = () => {
-    setBad(bad + 1)
-  }
+  const clickGood = () => setGood(good + 1)
+  
+  const clickNeutral = () => setNeutral(neutral + 1)
+  
+  const clickBad = () => setBad(bad + 1)
+  
 
-  const all = good + bad + neutral
-  const positive = all > 0 ? ((good / all) * 100).toFixed(10) : 0
-  const average = all > 0 ? ((good - bad)/all).toFixed(13) : 0
+  
   
 
 
  
   return (
     <div>
-      
       <h1>give feedback</h1>
-      <Button onClick={click_good} text='good'/>
-      <Button onClick={click_neutral} text='neutral'/>
-      <Button onClick={click_bad} text='bad'/>
-      <Statistics 
-        good={good} 
-        neutral={neutral} 
-        bad={bad} 
-        all={all} 
-        average={average}
-        positive={positive}
-       />
+      <Button handleClick={clickGood} text='good'/>
+      <Button handleClick={clickNeutral} text='neutral'/>
+      <Button handleClick={clickBad} text='bad'/>
+      <h1>statistics</h1>
+      <Statistics  good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
