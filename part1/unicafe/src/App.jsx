eca12bb1adab211493dcc54d557397e79eb0c323
Mouhamadou MBAYE <mams202002@yahoo.fr>
2026-03-26 12:23:06 +0100

    part1 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,31 +1,80 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
-    'The only way to go fast, is to go well.'
 
-  ]
-
-  const [selected, setSelected] = useState(0)
-  const nextAnecdote = () => {
-    const randomIndex = Math.floor(Math.random() * anecdotes.length)
-    setSelected(randomIndex)
-  }
+const StatisticLine = ({text, value}) => {
 
   return (
-    <div>
-      <p>{anecdotes[selected]}</p>
-      <button onClick={nextAnecdote}>next anecdote</button>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
     
+  )
+}
+
+
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + bad + neutral
+  const positive = all > 0 ? ((good / all) * 100) : 0
+  const average = all > 0 ? ((good - bad)/all) : 0
+  if (all === 0 ) return (
+        <p>No feedback given</p>
+  )
+  return (
+
+
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average.toFixed(2)} />
+        <StatisticLine text="positive" value={positive.toFixed(2) + "%"} />
+      </tbody>
+    </table>
+
+    
+    
+  )
+
+}
+
+const Button = ({handleClick, text}) => (
+<button onClick={handleClick}>{text}</button>)
+
+
+const App = () => {
+
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const clickGood = () => setGood(good + 1)
+  
+  const clickNeutral = () => setNeutral(neutral + 1)
+  
+  const clickBad = () => setBad(bad + 1)
+  
+
+  
+  
+
+
+ 
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={clickGood} text='good'/>
+      <Button handleClick={clickNeutral} text='neutral'/>
+      <Button handleClick={clickBad} text='bad'/>
+      <h1>statistics</h1>
+      <Statistics  good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
+
 
 export default App
